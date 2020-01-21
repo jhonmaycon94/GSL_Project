0037dee41983b883d2266e03ec62d23e0fb0f159
Josue Montalvao <josuemontalvao@gmail.com>
2020-01-21 17:24:05 -0300

    Added a dummy unity project

---
+++ sensori/unity/Carlos_Workshop/Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Button start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start.onClick.AddListener(StartGame);
+    }
+
+    void StartGame()
+    {
+        Debug.Log("The button was clicked!");
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ sensori/unity/Carlos_Workshop/Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float rotationSpeed = 100.0f;
+
+    public Text scoreText;
+
+    int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        // Make it move 10 meters per second instead of 10 meters per frame...
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(0, 0, translation);
+
+        // Rotate around our y-axis
+        transform.Rotate(0, rotation, 0);
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Target")
+        {
+            other.gameObject.SetActive(false);
+            score++;
+            scoreText.text = "Count: " + score;
+        }
+    }
+}
